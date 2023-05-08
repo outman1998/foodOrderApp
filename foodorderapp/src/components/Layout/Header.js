8a2dc205417c1c08a164dae2fcd352b0e33da511
outman1998 <outmanmounir98@gmail.com>
2023-05-08 14:27:55 +0200

    sws

--- foodorderapp/src/components/Layout/Header.js
+++ foodorderapp/src/components/Layout/Header.js
@@ -7,7 +7,7 @@ export default function Header(props) {
   return (
     <React.Fragment>
         <header className={classes.header}>
-            <h1>Meals4u</h1>
+            <h1>Meals App</h1>
             <HeaderCardButton handlingClick={props.onShowCart}/>
         </header>
         <div className={classes['main-image']}>
